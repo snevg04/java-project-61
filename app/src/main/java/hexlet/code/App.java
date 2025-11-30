020999062f49e4263b1f3b1f641b4d26e33d31ef
Evgenii <evg.sneg.117@yandex.ru>
2025-11-30 18:37:52 +1000

    updated src/main/java/hexlet/code/App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,5 +3,10 @@ package hexlet.code;
 class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+
+        System.out.println("May I have your name?");
+        var name = Cli.scanInput();
+        System.out.println("Hello, " + name + "!");
+
     }
 }
