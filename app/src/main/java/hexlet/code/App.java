b01f6fc1cefb67aef6fe97c26c562b768d3fc620
Evgenii <evg.sneg.117@yandex.ru>
2025-12-02 13:04:02 +1000

    add selector to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,11 +2,37 @@ package hexlet.code;
 
 class App {
     public static void main(String[] args) {
+        App.takeYourPick();
+    }
+
+    public static void greeting() {
         System.out.println("Welcome to the Brain Games!");
 
         System.out.println("May I have your name?");
         var name = Cli.scanInput();
         System.out.println("Hello, " + name + "!");
+    }
+
+    public static void takeYourPick() {
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 – Greet
+                0 – Exit
+                """
+        );
+
+        var pick = Cli.scanInput();
+
+        switch (pick) {
+            case "1":
+                App.greeting();
+                break;
+            case "0":
+                break;
+            default:
+                System.out.println("Invalid option");
+                App.takeYourPick();
+        }
 
     }
 }
