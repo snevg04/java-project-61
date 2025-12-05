89cfc57bccb523083c669c1e3d4ae505b3b4642f
Evgenii <evg.sneg.117@yandex.ru>
2025-12-06 00:19:24 +1000

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,46 +1,11 @@
 package hexlet.code;
 
-class App {
+import hexlet.code.games.*;
+
+public class App {
+
     public static void main(String[] args) {
-        App.takeYourPick();
+        Engine engine = new Engine();
+        engine.launch();
     }
-
-    public static String name;
-
-    public static void greeting() {
-        System.out.println("Welcome to the Brain Games!");
-
-        System.out.println("May I have your name?");
-        name = Cli.scanInput();
-        System.out.println("Hello, " + name + "!");
-    }
-
-    public static void takeYourPick() {
-        System.out.print("""
-                Please enter the game number and press Enter.
-                1 – Greet
-                2 - Even
-                0 – Exit
-                """
-        );
-        System.out.print("Your choice: ");
-
-        var pick = Cli.scanInput();
-
-        switch (pick) {
-            case "1":
-                App.greeting();
-                break;
-            case "2":
-                App.greeting();
-                Even.playGame();
-                break;
-            case "0":
-                break;
-            default:
-                System.out.println("Invalid option");
-                App.takeYourPick();
-        }
-    }
-
 }
