e1f0a69320df90ad7a7d3f187f7b491d441e2e41
Evgenii <evg.sneg.117@yandex.ru>
2025-12-06 23:58:34 +1000

    add difficulty variable to Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -5,13 +5,14 @@ import hexlet.code.RandomGenerator;
 
 public class Even implements BrainGamesAPI {
     private int question;
+    private int difficulty = 1000;
 
     public void getRules() {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
     }
 
     public void getQuestion() {
-        question = RandomGenerator.generateNumber(1000);
+        question = RandomGenerator.generateNumber(difficulty);
         System.out.println(question);
         getCorrectAnswer();
     }
