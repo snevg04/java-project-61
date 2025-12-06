6d18d5c6c1e4bf7b1043680aa4f43a3130ef273f
Evgenii <evg.sneg.117@yandex.ru>
2025-12-07 00:02:36 +1000

    add Progression.java to games

---
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,35 @@
+package hexlet.code.games;
+
+import hexlet.code.BrainGamesAPI;
+import hexlet.code.RandomGenerator;
+
+import java.util.Arrays;
+
+public class Progression implements BrainGamesAPI {
+    private int difficulty = 100;
+    private String answer;
+
+    public void getRules() {
+        System.out.println("What number is missing in the progression?");
+    }
+
+    public void getQuestion() {
+        var currentProgression = RandomGenerator.generateProgression(difficulty);
+        var missingProgressionItem = RandomGenerator.generateNumber(currentProgression.length);
+        String[] progressionString = new String[currentProgression.length];
+
+        for (int i = 0; i < currentProgression.length; i++) {
+            progressionString[i] = String.valueOf(currentProgression[i]);
+        }
+
+        answer = progressionString[missingProgressionItem];
+        progressionString[missingProgressionItem] = "..";
+
+        System.out.println(Arrays.toString(progressionString));
+
+    }
+
+    public String getCorrectAnswer() {
+        return answer;
+    }
+}
