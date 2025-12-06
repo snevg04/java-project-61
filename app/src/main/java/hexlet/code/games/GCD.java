e3e8346683bb700d3f1397316397859f63a6fb04
Evgenii <evg.sneg.117@yandex.ru>
2025-12-06 20:48:45 +1000

    add GCD.java to games

---
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,39 @@
+package hexlet.code.games;
+
+import hexlet.code.BrainGamesAPI;
+import hexlet.code.RandomGenerator;
+
+public class GCD implements BrainGamesAPI {
+    private int firstNumber;
+    private int secondNumber;
+
+    public void getRules() {
+        System.out.println("Find the greatest common divisor of given numbers");
+    }
+
+    public void getQuestion() {
+        firstNumber = RandomGenerator.generateNumber(100);
+        secondNumber = RandomGenerator.generateNumber(100);
+
+        System.out.println(firstNumber + " " + secondNumber);
+    }
+
+    public String getCorrectAnswer() {
+
+        if (secondNumber != 0) {
+            var a = firstNumber;
+            var b = secondNumber;
+
+            while (b != 0) {
+                var remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+
+            return Integer.toString(a);
+        } else {
+            return Integer.toString(secondNumber);
+        }
+    }
+
+}
