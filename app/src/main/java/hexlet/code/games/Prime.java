bb977af6cfc7f9fc8c4d6c3ef2de48f676098ecb
Evgenii <evg.sneg.117@yandex.ru>
2025-12-09 20:57:40 +1000

    add Prime.java to games

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,52 @@
+package hexlet.code.games;
+
+import hexlet.code.BrainGamesAPI;
+import hexlet.code.RandomGenerator;
+
+public class Prime implements BrainGamesAPI {
+   private boolean answer;
+   private int difficulty = 10;
+   private int question;
+
+    public void getRules() {
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+    }
+
+    public void getQuestion() {
+        question = RandomGenerator.generateNumber(difficulty);
+
+        System.out.println(question);
+    }
+
+    public boolean isPrime() {
+
+        if(question == 2) {
+            return true;
+        }
+
+        if(question < 2 || question % 2 == 0) {
+            return false;
+        }
+
+
+
+        var currentNumberSquareRoot = Math.sqrt(question);
+
+        for (var i = 3; i <= currentNumberSquareRoot; i += 2) {
+            if (question % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public String getCorrectAnswer() {
+        answer = isPrime();
+
+        if (answer) {
+            return "yes";
+        }
+        return "no";
+    }
+}
