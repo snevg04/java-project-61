5b25017849b55d879a4c6057d57edf11479ff3c8
Evgenii <evg.sneg.117@yandex.ru>
2025-12-23 14:44:32 +1000

    add Engine.ROUNDS support and separate "calculate" method

--- app/src/main/java/hexlet/code/games/Calculator.java
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -4,7 +4,6 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public final class Calculator {
-    private static final int DEFAULT_ROUNDS_NUMBER = 3;
     private static final int DEFAULT_ROUNDS_INFO_ITEMS = 2;
     private static final int DEFAULT_OPERATIONS_NUMBER = 3;
     private static final int DEFAULT_RANDOM_MAX_BOUND = 101;
@@ -16,40 +15,60 @@ public final class Calculator {
     }
 
     public static String[][] generateRounds() {
-        String[][] rounds = new String[DEFAULT_ROUNDS_NUMBER][DEFAULT_ROUNDS_INFO_ITEMS];
+        String[][] rounds = new String[Engine.ROUNDS][DEFAULT_ROUNDS_INFO_ITEMS];
 
-        for (var row = 0; row < rounds.length; row++) {
+        for (var i = 0; i < Engine.ROUNDS; i++) {
             int firstNumber = RANDOM.nextInt(DEFAULT_RANDOM_MAX_BOUND);
             int secondNumber = RANDOM.nextInt(DEFAULT_RANDOM_MAX_BOUND);
             int operationNumber = RANDOM.nextInt(DEFAULT_OPERATIONS_NUMBER);
-            int question;
-            String questionString;
+            String question;
             String answer;
-            var firstCol = 0;
-            var secondCol = firstCol + 1;
+            var firstColumn = 0;
+            var secondColumn = firstColumn + 1;
 
-            switch (operationNumber) {
-                case 0:
-                    question = firstNumber + secondNumber;
-                    questionString = firstNumber + " + " + secondNumber;
-                    break;
-                case 1:
-                    question = firstNumber - secondNumber;
-                    questionString = firstNumber + " - " + secondNumber;
-                    break;
-                default:
-                    question = firstNumber * secondNumber;
-                    questionString = firstNumber + " * " + secondNumber;
-                    break;
-            }
+            question = getQuestion(firstNumber, secondNumber, operationNumber);
+            answer = Integer.toString(calculate(firstNumber, secondNumber, operationNumber));
 
-            answer = Integer.toString(question);
-            rounds[row][firstCol] = questionString;
-            rounds[row][secondCol] = answer;
+            rounds[i][firstColumn] = question;
+            rounds[i][secondColumn] = answer;
         }
 
         return rounds;
     }
 
-}
+    public static String getQuestion(int firstNumber, int secondNumber, int operationNumber) {
+        String result;
 
+        switch (operationNumber) {
+            case 0:
+                result = firstNumber + " + " + secondNumber;
+                break;
+            case 1:
+                result = firstNumber + " - " + secondNumber;
+                break;
+            default:
+                result = firstNumber + " * " + secondNumber;
+                break;
+        }
+
+        return result;
+    }
+
+    public static int calculate(int firstNumber, int secondNumber, int operationNumber) {
+        int result;
+        switch (operationNumber) {
+            case 0:
+                result = firstNumber + secondNumber;
+                break;
+            case 1:
+                result = firstNumber - secondNumber;
+                break;
+            default:
+                result = firstNumber * secondNumber;
+                break;
+        }
+
+        return result;
+    }
+
+}
