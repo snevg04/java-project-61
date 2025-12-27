29ba24eeb18eea0adcd7d4d2ba23c31c817bc494
Evgenii <evg.sneg.117@yandex.ru>
2025-12-27 22:34:57 +1000

    make "random" local variable

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -6,21 +6,22 @@ import java.util.Random;
 public final class Even {
     private static final int DEFAULT_ROUNDS_INFO_ITEMS = 2;
     private static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-    private static final Random RANDOM = new Random();
 
     public static void run() {
         Engine.run(RULES, generateRounds());
     }
 
     public static String[][] generateRounds() {
+        Random random = new Random();
         String[][] rounds = new String[Engine.ROUNDS][DEFAULT_ROUNDS_INFO_ITEMS];
 
         for (var i = 0; i < Engine.ROUNDS; i++) {
-            int question = RANDOM.nextInt();
-            String questionString = Integer.toString(question);
-            String answer = question % 2 == 0 ? "yes" : "no";
+            int question = random.nextInt();
             var firstColumn = 0;
             var secondColumn = firstColumn + 1;
+
+            String questionString = Integer.toString(question);
+            String answer = question % 2 == 0 ? "yes" : "no";
 
             rounds[i][firstColumn] = questionString;
             rounds[i][secondColumn] = answer;
