dde1e94546b5b2cc703e99c37a3da3c9c4c976c7
Evgenii <evg.sneg.117@yandex.ru>
2025-12-27 22:38:51 +1000

    make "random" local variable

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -8,26 +8,23 @@ public final class GCD {
     private static final int DEFAULT_RANDOM_MIN_BOUND = 1;
     private static final int DEFAULT_RANDOM_MAX_BOUND = 101;
     private static final String RULES = "Find the greatest common divisor of given numbers.";
-    public static final Random RANDOM = new Random();
 
     public static void run() {
         Engine.run(RULES, generateRounds());
     }
 
     public static String[][] generateRounds() {
+        Random RANDOM = new Random();
         String[][] rounds = new String[Engine.ROUNDS][DEFAULT_ROUNDS_INFO_ITEMS];
 
         for (var i = 0; i < Engine.ROUNDS; i++) {
-            String question;
-            String answer;
             var firstColumn = 0;
             var secondColumn = firstColumn + 1;
             int firstNumber = RANDOM.nextInt(DEFAULT_RANDOM_MIN_BOUND, DEFAULT_RANDOM_MAX_BOUND);
             int secondNumber = RANDOM.nextInt(DEFAULT_RANDOM_MIN_BOUND, DEFAULT_RANDOM_MAX_BOUND);
 
-            question = firstNumber + " " + secondNumber;
-
-            answer = Integer.toString(calculateGCD(firstNumber, secondNumber));
+            String question = firstNumber + " " + secondNumber;
+            String answer = Integer.toString(calculateGCD(firstNumber, secondNumber));
 
             rounds[i][firstColumn] = question;
             rounds[i][secondColumn] = answer;
